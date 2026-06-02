b4044056932f21da34e2360ce8aa7da454602c02
TajmulHossainDev <tajmulhossain6991@gmail.com>
2026-06-02 18:04:42 +0300

    implement jwt auth middleware

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 dotenv.config();
 
 const uri = process.env.MONGODB_URI;
@@ -26,6 +27,39 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+const JWKS = createRemoteJWKSet(
+  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`),
+);
+
+
+const verifyToken = async (req, res, next) => {
+  let token = req.cookies?.auth_token;
+  if (!token) {
+    const authHeader = req?.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).json({ message: "unauthorized" });
+    }
+    token = authHeader.split(" ")[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: "unauthorized" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+
+    req.user = payload;
+    next();
+  } catch (error) {
+    return res.status(403).json({
+      message: "Forbidden",
+      error: error.message,
+    });
+  }
+};
+
 
 app.get("/", (req, res) => {
   res.send("SportNest server is running...");
